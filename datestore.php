f55271be716ee45b2ab0a11a26665fcfbb3f8ee0
Kyle <kkarpack@uw.edu>
2013-01-15 14:43:51 -0800

    API updates

--- datestore.php
+++ datestore.php
@@ -1,18 +1,33 @@
 <?php
-session_start();
 
 //reject direct access eventually
-//if (isset($_SERVER['HTTP_REFERER'])) {
-if (isset($_SESSION["name"])  && isset($_GET["start"]) && isset($_GET["end"])) {
-	$start = $_GET["start"];
-	$end = $_GET["end"];
+//if (isset($_SERVER['HTTP_REFERER']) && isset($_SESSION["name"])) {
+	session_start();
 	$user = $_SESSION["name"];
 	$connection = mysql_select_db("steve", mysql_connect("localhost", "steve", "password"));
-	mysql_query("INSERT INTO availability VALUES ('$user','$start','$end')");
-	echo ("Stored a start time of " . $start . " and an end time of " . $end . " successfully for " . $user);
-} else {
-	echo "broked";
-	//header("Location: index.php"); //reject direct access to this page (low security)
-}
+	if ($_SERVER['REQUEST_METHOD'] == "POST") { //logging user input
+		if (isset($_POST["start"]) && isset($_POST["end"])) {
+			$start = $_POST["start"];
+			$end = $_POST["end"];
+			mysql_query("INSERT INTO availability VALUES ('$user','$start','$end')");
+			echo ("Stored a start time of " . $start . " and an end time of " . $end . " successfully for " . $user);
+		} else {
+			header("HTTP/1.0 400 Invalid Request");
+		}
+	} else { //get request, load up the calendar with JSON data
+		$query = mysql_query("SELECT start, end FROM availability WHERE username = '$user'");
+		$events = array();
+		for ($x = 0, $numrows = mysql_num_rows($query); $x < $numrows; $x++) {
+			$row = mysql_fetch_assoc($query);
+			// array in the form (startdatetime, enddatetime)
+			$events[] = array($row["start"], $row["end"]); 			
+		}
+		//var_dump($events);
+		$response = json_encode($events); 
+		echo $response;
+	}
+//} else {
+	// header("Location: index.php"); //reject direct access to this page (low security)
+// }
 
 ?>
--- js.js
+++ js.js
@@ -22,11 +22,21 @@ $(window).load(function() {
 function store(start,end) {
 	start = toDateString(start);
 	end = toDateString(end);
-	$.get("datestore.php",
-				{ "start": start,
-					"end": end 
-				}
-		);
+	//
+	$.ajax({
+		type: "POST",
+		url: "datestore.php",
+		data: { "start": start,
+				 "end": end 
+			}
+		});
+	$.ajax({
+		type: "GET",
+		url: "datestore.php",
+		success: function(data) {
+			console.log($.parseJSON(data));
+		}
+		});
 }
 
 // utility functions
@@ -34,6 +44,7 @@ function logout() {
 	window.location.href = "logout.php";
 }
 
+// converts a JS date object to a MySQL datetime string
 function toDateString(start) {
 	return start.getUTCFullYear() + '-' + ('00' + (start.getUTCMonth()+1)).slice(-2) + '-' + start.getUTCDate() + ' ' + ('00' + start.getUTCHours()).slice(-2) + ':' + ('00' + start.getUTCMinutes()).slice(-2) + ':' + ('00' + start.getUTCSeconds()).slice(-2);
 }
