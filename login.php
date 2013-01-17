f3d7dceafb043bd04e5b7cbf116041ae27602204
Cory Shiraishi <cjtshiraishi@gmail.com>
2013-01-16 16:42:18 -0800

    added salting and hashing to login

--- login.php
+++ login.php
@@ -1,16 +1,14 @@
 <?php
-// TO DO: implement hashing and salting!
 if (isset($_SERVER['HTTP_REFERER'])) {
 
 	$connection = mysql_select_db("steve", mysql_connect("localhost", "steve", "password"));
-
-	$name = get_parameter("name");
+	
+	$mail= get_parameter("email");
 	$pw = get_parameter("password");
 
-	$test = mysql_fetch_row(mysql_query("SELECT password FROM users_test WHERE username = '$name'"));
-	if ($test != false && $pw == $test[0]) { //if name and pw are correct
-		session_start();
-		$_SESSION["name"] = $name; //start session with name (i.e. kkarpack)
+	$test = mysql_fetch_assoc(mysql_query("SELECT * FROM users_test WHERE email = '$email'"));
+	if ($test != false && hash("sha256",$test['pass_salt'].$pw == $test['pass_hash'])) { //if username/pw are correct
+		$_SESSION["name"] = $email; //start session with name (i.e. kkarpack)
 		header("Location: index.php"); //redirect to the list
 		die();
 	} else {
@@ -23,9 +21,9 @@ if (isset($_SERVER['HTTP_REFERER'])) {
 }
 
 //helper function to check for valid data passed
-function get_parameter($name) {
-	if (isset($_POST[$name])) {		//if name/pw are valid parameters
-		return mysql_real_escape_string($_POST[$name]);		//return those
+function get_parameter($param) {
+	if (isset($_POST[$param])) {		//if name/pw are valid parameters
+		return mysql_real_escape_string($_POST[$param]);		//return those
 	} else { 						//if nothing posted
 		header("HTTP/1.1 400 Invalid Request");
 		die("HTTP/1.1 400 Invalid Request - you forgot to pass the proper parameters");
